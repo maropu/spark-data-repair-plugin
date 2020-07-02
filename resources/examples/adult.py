9595bc0d3159f344f79bc4b80c4f2c48e458bd54
Takeshi Yamamuro <yamamuro@apache.org>
2020-07-02 17:32:49 +0900

    Add adult test data

---
+++ resources/examples/adult.py
@@ -0,0 +1,54 @@
+# Loads a target data then defines tables for it
+spark.read \
+  .option("header", True) \
+  .csv("./testdata/adult.csv") \
+  .write \
+  .saveAsTable("adult")
+
+scavenger.repair().misc() \
+  .setDbName("default") \
+  .setTableName("adult") \
+  .setRowId("tid") \
+  .flatten() \
+  .write \
+  .saveAsTable("adult_flatten")
+
+spark.table("adult").show(1)
+spark.table("adult_flatten").show(1)
+
+# Loads a ground truth data then defines tables for it
+spark.read \
+  .option("header", True) \
+  .csv("./testdata/adult_clean.csv") \
+  .write \
+  .saveAsTable("adult_clean")
+
+spark.table("adult_flatten") \
+  .join(spark.table("adult_clean"), ["tid", "attribute"], "inner") \
+  .where("not(value <=> correct_val)") \
+  .write \
+  .saveAsTable("error_cells_ground_truth")
+
+spark.table("adult_clean").show(1)
+spark.table("error_cells_ground_truth").show(1)
+
+# Detects error cells then repairs them
+repaired_df = scavenger.repair() \
+  .setDbName("default") \
+  .setTableName("adult") \
+  .setRowId("tid") \
+  .run(return_repair_candidates=True)
+
+# Computes performance numbers (precision & recall)
+#  - Precision: the fraction of correct repairs, i.e., repairs that match
+#    the ground truth, over the total number of repairs performed
+#  - Recall: correct repairs over the total number of errors
+pdf = repaired_df.join(spark.table("adult_clean"), ["tid", "attribute"], "inner")
+rdf = repaired_df.join(spark.table("error_cells_ground_truth"), ["tid", "attribute"], "right_outer")
+
+precision = pdf.where("repaired <=> correct_val").count() / pdf.count()
+recall = rdf.where("repaired <=> correct_val").count() / rdf.count()
+f1 = (2.0 * precision * recall) / (precision + recall)
+
+print("Precision=%s Recall=%s F1=%s" % (precision, recall, f1))
+
