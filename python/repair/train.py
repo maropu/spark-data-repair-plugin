5a7bbe30857aa75b84484db210decf1c82d4c649
Takeshi Yamamuro <yamamuro@apache.org>
2021-11-17 21:56:32 +0900

    Fix test faiures

--- python/repair/train.py
+++ python/repair/train.py
@@ -145,7 +145,7 @@ def _build_lgb_model(X: pd.DataFrame, y: pd.Series, is_discrete: bool, num_class
 
     def _objective(params: Dict[str, Any]) -> float:
         model = _create_model(params)
-        fit_params = {
+        fit_params: Dict[str, str] = {
             # TODO: Raises an error if a single regressor is used
             # "categorical_feature": "auto",
         }
